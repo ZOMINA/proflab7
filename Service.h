6af39cc231ff2edeab67778c9b3a444b5dcdc121
”Erik <erikuskov1@gmail.com>
2024-04-29 09:16:16 +0300

    add servise

---
+++ Service.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Weather.h"
+#include <string>
+using namespace std;
+class Service
+{
+private:
+public:
+	virtual Weather getWeather(std::string s) = 0;
+	~Service() {
+	}
+};
+
